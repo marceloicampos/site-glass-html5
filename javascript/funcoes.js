12a4413636909ed65ab6032b1081ede9ee402281
marceloicampos <marcelo.ildefonso@hotmail.com>
2021-02-22 15:04:06 -0300

    Update fale-conosco e Ajustes Finais

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,13 +1,17 @@
 function mudaFoto (foto) {
-    document.getElementById("icone").src = foto;
+    document.getElementById("icone").src = foto
 }
 
-var quebrada = false;
+var quebrada = false
 function mudaLampada(tipo) {
     if (!quebrada) {
-        document.getElementById("luz").src = "./imagens/" + tipo + ".jpg";
+        document.getElementById("luz").src = "./imagens/" + tipo + ".jpg"
     if (tipo == 'lampada-quebrada') {
-        quebrada = true;
+        quebrada = true
+        }
     }
+    if (tipo == 'lampada-alert') {
+        alert("Você que quebrou a lâmpada, CLIQUE EM OK PARA CONSERTAR")
+        window.location.reload()
     }
 }
